503d3a16c556ef4ea84373ec3ab5ae7ee46e7474
pranavvyas20 <pranavvyas2003@gmail.com>
2024-03-29 20:09:32 +0530

    test commit

--- app/src/main/java/com/example/wakatime/MainActivity.kt
+++ app/src/main/java/com/example/wakatime/MainActivity.kt
@@ -15,6 +15,7 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+            //
         }
     }
 }
