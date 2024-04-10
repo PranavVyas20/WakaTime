de94dfe115841b759daae793aaacc86c09153ebf
pranavvyas20 <pranavvyas2003@gmail.com>
2024-04-11 01:32:13 +0530

    Add functions for user authorization and token retrieval

--- app/src/main/java/com/example/wakatime/data/remote/WakaApi.kt
+++ app/src/main/java/com/example/wakatime/data/remote/WakaApi.kt
@@ -9,4 +9,17 @@ interface WakaApi {
     suspend fun getWakaUserSummary(
         @Query("api_key") apiKey: String = "waka_30159012-1a44-427c-be48-5b39835409c8"
     ): WakaUserSummaryResponse
+
+    @GET("/oauth/authorize")
+    suspend fun authorizeUserAccount()
+
+    @FormUrlEncoded
+    @POST("/oauth/token")
+    suspend fun getAuthToken(
+        @Field("client_id") clientId: String = "IYe2gJjSAF0A388BRHIlZP2O",
+        @Field("client_secret") clientSecret: String = "waka_sec_TMTLUVTlBnyFTppzpauScbmzMJa9x6TvgSkBFQzbKzRPCGSoY84myMmGq1tFFAyIb8pRCcgvCkyCf39w",
+        @Field("redirect_uri") redirectUri: String = "https://www.youtube.com/",
+        @Field("grant_type") grantType: String = "refresh_token",
+        @Field("refresh_token") refreshToken: String = "waka_ref_bCTl2uugbjikmFnf4wylNFY5yIgqJAOl0rQd7ENL0CPaqzhwtgpzWBBIdg91iPnzTNBchbymDpZQUd0X"
+    ): String
 }
