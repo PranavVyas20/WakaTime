2c85d5fcf3e7f8b041384c2201c1affc8b05daec
pranavvyas20 <pranavvyas2003@gmail.com>
2024-04-14 17:31:39 +0530

    add interceptor and client to retrofit builder

--- app/src/main/java/com/example/wakatime/di/AppModule.kt
+++ app/src/main/java/com/example/wakatime/di/AppModule.kt
@@ -1,12 +1,15 @@
 package com.example.wakatime.di
 
 import com.example.wakatime.data.remote.WakaApi
+import com.example.wakatime.data.remote.WakaInterceptor
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.converter.scalars.ScalarsConverterFactory
 import javax.inject.Singleton
 
 
@@ -16,10 +19,15 @@ object AppModule {
 
     @Provides
     @Singleton
-    fun provideWeatherApi(): WakaApi {
+    fun provideWeatherApi(wakaInterceptor: WakaInterceptor): WakaApi {
+        val client = OkHttpClient.Builder()
+            .addInterceptor(wakaInterceptor)
+            .build()
         return Retrofit.Builder()
             .baseUrl("https://wakatime.com/")
+            .addConverterFactory(ScalarsConverterFactory.create())
             .addConverterFactory(GsonConverterFactory.create())
+            .client(client)
             .build()
             .create(WakaApi::class.java)
     }
