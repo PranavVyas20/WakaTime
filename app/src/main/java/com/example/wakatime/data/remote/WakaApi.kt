c5f5e3c368cab86fdd80fffcd54b95ee471a7495
pranavvyas20 <pranavvyas2003@gmail.com>
2024-04-18 01:01:31 +0530

    add scope in parameter and remove api key from auth url endpoint

--- app/src/main/java/com/example/wakatime/data/remote/WakaApi.kt
+++ app/src/main/java/com/example/wakatime/data/remote/WakaApi.kt
@@ -1,13 +1,17 @@
 package com.example.wakatime.data.remote
 
 import com.example.wakatime.data.model.WakaUserSummaryResponse
+import retrofit2.http.Field
+import retrofit2.http.FormUrlEncoded
 import retrofit2.http.GET
+import retrofit2.http.POST
 import retrofit2.http.Query
 
 interface WakaApi {
+
     @GET("/api/v1/users/current/summaries?start=today&end=today")
     suspend fun getWakaUserSummary(
-        @Query("api_key") apiKey: String = "waka_30159012-1a44-427c-be48-5b39835409c8"
+        @Query("scope") scope: String = "read_summaries",
     ): WakaUserSummaryResponse
 
     @GET("/oauth/authorize")
